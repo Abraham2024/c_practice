462942aeb913287780aa4bf4d0a6681646c6eba0
drabraham2024 <ibrahimjunior08@gmail.com>
2023-04-12 03:35:45 -0700

    c programming

---
+++ basics/README.md
@@ -0,0 +1,1 @@
+this file is to practice the basics of c programming
---
+++ basics/print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- prints main function
+ * description - prints some words each on a new line
+ * return - 0
+ */
+int main (void)
+{
+	printf("this is my first practice code\n");
+		puts("i love to code in c programming\n");
+		return(0);
+}
+
