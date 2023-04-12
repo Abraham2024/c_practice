41df0c7ca0b7f1c9fb2ce94d4f1157c7e08d7a3d
drabraham2024 <ibrahimjunior08@gmail.com>
2023-04-12 03:47:42 -0700

    c programming

--- basics/print.c
+++ basics/print.c
@@ -9,6 +9,9 @@ int main (void)
 {
 	printf("this is my first practice code\n");
 		puts("i love to code in c programming\n");
+		puts("my name is Abraham \"AB\" for short\n");
+		printf("i love programming \t");
+		printf("i love coding\n");
 		return(0);
 }
 
