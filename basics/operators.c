346273229aa30186be6a500cb00593d1439b536d
drabraham2024 <ibrahimjunior08@gmail.com>
2023-04-14 04:01:57 -0700

    'c programming'

---
+++ basics/operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- main entry point
+ *
+ * description - prints the value of operators
+ *
+ * return - 0
+ */
+int main(void)
+{
+	int j = 12;
+	int k = 5;
+	int mod = j%k;
+	
+	printf("%d\n", mod);
+		return(0);
+}
+
---
+++ basics/operators1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main entry function
+ *
+ * description - prints the incremented value
+ * adds 1 to the value
+ *
+ * return - 0
+ */
+
+int main(void)
+{
+	int i = 45;
+		printf("%i\n", ++i);
+		return(0);
+}
