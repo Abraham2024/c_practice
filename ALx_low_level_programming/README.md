e597d1f5c79ffbadb361b2fda6e98ae0a87e920f
Ibrahim Muhammed Amanokhai <ibrahimjunior08@gmail.com>
2023-04-18 03:55:04 -0700

    my_commit

---
+++ ALx_low_level_programming/README.md
@@ -0,0 +1,1 @@
+a directory for alx low level programming practice
---
+++ ALx_low_level_programming/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description - prints a string
+ *
+ * return - 0
+ *
+ */
+int main(void)
+{
+	printf("programming is a multilingual language\n");
+		return(0);
+}
